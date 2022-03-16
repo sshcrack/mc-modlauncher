a61ef0b9d463397ebdfaefc45be06950bdbbfca5
sshcrack <hendrik.lind777@gmail.com>
2022-03-16 22:10:59 +0100

    reduced log files

--- src/interfaces/mainLogger.ts
+++ src/interfaces/mainLogger.ts
@@ -1,5 +1,5 @@
 import electronLog from "electron-log"
-electronLog.transports.file.maxSize = 1024 * 1024 * 20
+electronLog.transports.file.maxSize = 1024 * 1024 * 5
 
 export class MainLogger {
     static formatScope(arr: string[]) {
