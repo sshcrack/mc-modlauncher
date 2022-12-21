201e6ab83a8609596a01ef416cddadf85cc7471e
sshcrack <hendrik.lind777@gmail.com>
2022-12-22 00:20:58 +0100

    fixed typo which caused fabric not to install

--- src/backend/InstallManager/processors/fabric/FabricDownloader.ts
+++ src/backend/InstallManager/processors/fabric/FabricDownloader.ts
@@ -1,7 +1,7 @@
 import { MainGlobals } from '../../../../Globals/mainGlobals';
 import { ModpackInfo, Version } from '../../../../interfaces/modpack';
 import { AdditionalOptions } from '../../event/Processor';
-import { getForgeInstallerZip } from '../../General/mcBase';
+import { getFabricInstallerJar } from '../../General/mcBase';
 import { Downloader } from '../base/Downloader';
 import { getFabricUrl } from './file';
 
@@ -11,7 +11,7 @@ export class FabricDownloader extends Downloader {
     constructor(id: string, config: ModpackInfo, version: Version, options: AdditionalOptions) {
         super(id, config, version, {
             ...options,
-            destination: getForgeInstallerZip(MainGlobals.getInstallDir(), id, version),
+            destination: getFabricInstallerJar(MainGlobals.getInstallDir()),
             url: getFabricUrl(),
             overwrite: false,
             messages: {
